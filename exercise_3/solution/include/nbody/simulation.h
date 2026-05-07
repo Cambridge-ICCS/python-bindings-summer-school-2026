16282a5af459eccd539c5d76c7ffd76c8983417f
M. A. Kowalski <mak60@cam.ac.uk>
2026-05-07 11:23:40 +0100

    add clang-format config and apply

--- exercise_3/solution/include/nbody/simulation.h
+++ exercise_3/solution/include/nbody/simulation.h
@@ -11,42 +11,42 @@ namespace nbody {
 //   length: astronomical unit [AU]
 //   time: years
 namespace constants {
-    constexpr double gravitation_constant = 4.0 * M_PI * M_PI;
-    constexpr double eps = 1.0e-6;
-    constexpr double earth_mass = 3.003e-6;
-    constexpr double solar_mass = 1.0;
-}
+  constexpr double gravitation_constant = 4.0 * M_PI * M_PI;
+  constexpr double eps = 1.0e-6;
+  constexpr double earth_mass = 3.003e-6;
+  constexpr double solar_mass = 1.0;
+} // namespace constants
 
 class Simulation {
 public:
-    struct Particle {
-        double x, y, z;      // Position components
-        double vx, vy, vz;   // Velocity components
-        double mass;         // Particle mass
-    };
+  struct Particle {
+    double x, y, z;    // Position components
+    double vx, vy, vz; // Velocity components
+    double mass;       // Particle mass
+  };
 
-    Simulation(double timestep, std::vector<Particle> particles);
+  Simulation(double timestep, std::vector<Particle> particles);
 
-    // Method to progress the simulation by specified time
-    void progress(double time);
+  // Method to progress the simulation by specified time
+  void progress(double time);
 
-    const auto& getParticles() const { return particles_; }
+  const auto &getParticles() const { return particles_; }
 
-    auto numParticles() const {return particles_.size(); }
+  auto numParticles() const { return particles_.size(); }
 
-    // Print particle data in CSV format to provided output stream
-    void printCSV(std::ostream& os) const;
+  // Print particle data in CSV format to provided output stream
+  void printCSV(std::ostream &os) const;
 
-    // Get elapsed time
-    double getElapsedTime() const;
+  // Get elapsed time
+  double getElapsedTime() const;
 
 private:
-    double timestep_;
-    std::vector<Particle> particles_;
-    double elapsed_time_;
+  double timestep_;
+  std::vector<Particle> particles_;
+  double elapsed_time_;
 
-    void leapFrogStep();
-    void validateParticles() const;
+  void leapFrogStep();
+  void validateParticles() const;
 };
 
 } // namespace nbody
